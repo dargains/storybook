b1a8879d672e57f3f60d00b5df666968e0252ea2
André Dargains <andre.dargains@gmail.com>
2020-03-03 14:39:07 +0000

    Initial commit

---
+++ src/moduleA/moduleA.js
@@ -0,0 +1,8 @@
+// document.querySelector('article').innerHTML = "haha"
+console.log('oi');
+function mudar() {
+  const oi = document.getElementById('oi');
+  console.log(oi);
+  oi.innerText = 'oi'
+}
+mudar()
---
+++ src/moduleA/moduleA.stories.js
@@ -0,0 +1,7 @@
+import html from './moduleA.html'
+import './moduleA.css'
+import './moduleA.js'
+
+export default { title: 'Module A' };
+
+export const withText = () => html;
