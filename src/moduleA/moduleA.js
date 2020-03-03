256e67367c7d09f3b471df0f376a2421307681fc
André Dargains <andre.dargains@gmail.com>
2020-03-03 18:04:26 +0000

    UPD: moduleA changes;

--- src/moduleA/moduleA.js
+++ src/moduleA/moduleA.js
@@ -1,8 +1,7 @@
-// document.querySelector('article').innerHTML = "haha"
+
 console.log('oi');
 function mudar() {
   const oi = document.getElementById('oi');
   console.log(oi);
-  oi.innerText = 'oi'
 }
 mudar()
