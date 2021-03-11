f25698d00687d0a573e41a8e27aae44692e022b4
André Dargains <andre.dargains@gmail.com>
2021-03-11 11:57:13 +0000

    UPD: args;

--- src/moduleB/moduleB.stories.js
+++ src/moduleB/moduleB.stories.js
@@ -6,18 +6,27 @@ export default {
     },
     disabled: {
       control: { type: 'boolean'}
+    },
+    items: {
+      control: { type: 'array' }
     }
   }
 }
 
-const Template = ({title, disabled}) => `
+const Template = ({title, disabled, items}) => `
+<section>
   <p style="color: ${disabled ? 'gray' : 'black'}">${title}</p>
+  <ul>
+    ${items.map(item => '<li>' + item + '</li>').join('')}
+  </ul>
+</section>
 `
 
 export const WithText = Template.bind({})
 WithText.args = {
   title: 'Cenas',
-  disabled: false
+  disabled: false,
+  items: ['item 1', 'item 2']
 }
 
 export const WithoutText = Template.bind({})
