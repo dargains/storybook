fd0d1ba376ce7cc442609eb94535bd25b0dec8ca
André Dargains <andre.dargains@gmail.com>
2021-03-11 11:49:45 +0000

    UPD: storybook config;

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,35 +1,12 @@
 import { addParameters } from "@storybook/html";
 
-export const globalTypes = {
-  xray: {
-    name: 'Grid',
-    description: 'Grid system',
-    defaultValue: false,
-    toolbar: {
-      icon: 'grid',
-      items: ['off', 'on'],
-    },
-  },
-};
-
 addParameters({
-  docs: {
-    inlineStories: true,
-    iframeHeight: '60px',
-  },
-  backgrounds: {
-    values: [
-      { name: 'm-01', value: '#E51B15' },
-      { name: 'm-02', value: '#222222' },
-    ]
-  },
   a11y: {
     element: '#root',
     config: {},
     options: {},
     manual: false,
   },
-  viewMode: "docs",
   viewport: {
     viewports: {
       sm: {
